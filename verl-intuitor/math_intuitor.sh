ac13c9bf9c02cce3d6bfda6bbf777e6ea590fce0
baibizhe <33809209+baibizhe@users.noreply.github.com>
2025-09-09 18:31:53 +0800

    Update math_intuitor.sh

--- verl-intuitor/math_intuitor.sh
+++ verl-intuitor/math_intuitor.sh
@@ -3,6 +3,7 @@ mkdir ~/data/
 cp -r ./data/math  ~/data/math
 # pip install -e .
 # pip install antlr4-python3-runtime==4.13.2
+pip install -U math_verify
 export ACCELERATE_LOG_LEVEL=info
 export HYDRA_FULL_ERROR=1
 
@@ -15,8 +16,8 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 ray stop --force
 
 # export RAY_MASTER_PORT=6379
-export PET_NODE_RANK=0
-export WORLD_SIZE=1
+# export PET_NODE_RANK=0
+# export WORLD_SIZE=1
 # if [ "$PET_NODE_RANK" -eq 0 ]; then
 #     ray start --head --port=$RAY_MASTER_PORT --dashboard-host=0.0.0.0 --num-gpus 8
 #     echo "Started Ray head node at $NODE_IP"
@@ -31,13 +32,11 @@ export WORLD_SIZE=1
 # Wait for 30 seconds to ensure the Ray cluster is ready
 # sleep 30
 
-# ========= 基本配置 =========
 PROJECT_NAME="self_rl_math"
 RUN_NAME="grpo_mv_naive_3b"
 TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 EXPERIMENT_NAME="${PROJECT_NAME}-${RUN_NAME}-${TIMESTAMP}"
 
-# ========= 输出目录 =========
 SAVE_CHECKPOINT_DIR="output_dir"
 EXP_DIR="${SAVE_CHECKPOINT_DIR}/${PROJECT_NAME}/${EXPERIMENT_NAME}"
 # mkdir 
@@ -59,7 +58,7 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     data.max_response_length=3072 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=/inspire/hdd/project/robot-dna/sujiadi-p-sujiadi/baibizhe-tmp/ckpts/llm/Qwen2.5-3B  \
+    actor_rollout_ref.model.path='your_llm_path'  \
     actor_rollout_ref.model.use_fused_kernels=False \
     actor_rollout_ref.actor.optim.lr=1e-5 \
     actor_rollout_ref.actor.optim.warmup_style=cosine \
@@ -98,4 +97,3 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     +trainer.rl_logging_board_dir=${SAVE_CHECKPOINT_DIR}/logs/rl_logging_board \
     trainer.total_epochs=4   > running_logs/${today}/${EXPERIMENT_NAME}.log 2>&1
 
-python /inspire/hdd/project/robot-dna/sujiadi-p-sujiadi/baibizhe-tmp/do_gpu_work.py
